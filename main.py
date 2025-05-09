9506f292be1f1caab03ee9d59f222c924c8b7f96
MuhammadHuzaifaBukhari <syedhuzaifabukhari80@gmail.com>
2025-05-09 20:01:25 +0500

    logs work started

--- main.py
+++ main.py
@@ -7,6 +7,14 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 import time
 import sys
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    filename='automation.log',
+    filemode='a'
+)
+from selenium.common.exceptions import TimeoutException
 
 from pages.landing_page.landingPage import test_landing_page
 from pages.auth.auth import sign_in
@@ -46,7 +54,7 @@ def hover_over_roles(hover_roles_elements):
             driver.execute_script("arguments[0].scrollIntoView({behavior: 'smooth', block: 'center'});", element)
             time.sleep(1)  # Optional: wait a bit for any animation or lazy-load
             actions.move_to_element(element).perform()
-            time.sleep(2)  # Optional: to observe the hover effect
+            time.sleep(1)  # Optional: to observe the hover effect
         except Exception as e:
             print(f"An error occurred while hovering over the element: {e}")
 
@@ -63,80 +71,78 @@ hover_roles_elements = [
 
 #------------------main landing page-------------#
 #---------learn more---------#
-# learnMore = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[class='styles_bannerBtns__FYrdf'] button[class='p-button p-component p-button-info']"))).click()
-# time.sleep(10)
-#
-# #read more/read less
-# try:
-#     # Click on "Read more..."
-#     read_more = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Read more...']"))
-#     )
-#     driver.execute_script("arguments[0].scrollIntoView();", read_more)
-#     actions.move_to_element(read_more).click().perform()
-#     print("Clicked 'Read more...'")
-#
-#     # Optional wait to allow content to expand
-#     time.sleep(2)
-#
-#     # Click on "Read less"
-#     read_less = WebDriverWait(driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, "(//span[normalize-space()='Read less'])[1]"))
-#     )
-#     driver.execute_script("arguments[0].scrollIntoView();", read_less)
-#     actions.move_to_element(read_less).click().perform()
-#     print("Clicked 'Read less'")
-#
-#     # Wait and go back if needed
-#     time.sleep(2)
-#     driver.back()
-#
-# except TimeoutException as te:
-#     print("Timeout waiting for element:", te)
-# except Exception as e:
-#     print("An error occurred:", e)
+learnMore = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"div[class='styles_bannerBtns__FYrdf'] button[class='p-button p-component p-button-info']"))).click()
+time.sleep(10)
+
+#read more/read less
+try:
+    # Click on "Read more..."
+    read_more = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "//span[normalize-space()='Read more...']"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", read_more)
+    actions.move_to_element(read_more).click().perform()
+    logging.info("learn more link clicked via JavaScript.")
+    time.sleep(2)
+
+    # Click on "Read less"
+    read_less = WebDriverWait(driver, 10).until(
+        EC.element_to_be_clickable((By.XPATH, "(//span[normalize-space()='Read less'])[1]"))
+    )
+    driver.execute_script("arguments[0].scrollIntoView();", read_less)
+    actions.move_to_element(read_less).click().perform()
+    print("Clicked 'Read less'")
+
+    # Wait and go back if needed
+    time.sleep(2)
+    driver.back()
+
+except TimeoutException:
+    logging.error("Read more and read less aren't found on the page.")
+except Exception as e:
+    print("An error occurred:", e)
 
 # #-------hover on roles---------#
-# hover_over_roles(hover_roles_elements)
+hover_over_roles(hover_roles_elements)
 #
-# #---------View all---------#
-# viewAllSubjects = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, "(//button[@aria-label='View all'])[1]"))
-# )
-# driver.execute_script("arguments[0].scrollIntoView();", viewAllSubjects)
-# actions.move_to_element(viewAllSubjects).click().perform()
-# print("Clicked 'View all'")
-# time.sleep(5)
+#---------View all---------#
+viewAllSubjects = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "(//button[@aria-label='View all'])[1]"))
+)
+driver.execute_script("arguments[0].scrollIntoView();", viewAllSubjects)
+actions.move_to_element(viewAllSubjects).click().perform()
+print("Clicked 'View all'")
+time.sleep(5)
 # #featured Subjects(Business and Economics)
-# businessEcoomicsElement = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, "//body/div[@id='__next']/main[@class='__className_d65c78']/div/div/div[@class='p-card p-component styles_cardStyle___86BP']/div[@class='p-card-body']/div[@class='p-card-content']/div[@class='grid']/div[1]/a[1]/div[1]/div[1]"))
+# businessEconomicsElement = WebDriverWait(driver, 10).until(
+#     EC.element_to_be_clickable((By.CSS_SELECTOR, "body > div:nth-child(1) > main:nth-child(2) > div:nth-child(4) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > div:nth-child(1) > div:nth-child(1)"))
 # )
-# driver.execute_script("arguments[0].scrollIntoView();", businessEcoomicsElement)
-# actions.move_to_element(businessEcoomicsElement).click().perform()
+# driver.execute_script("arguments[0].scrollIntoView();", businessEconomicsElement)
+# actions.move_to_element(businessEconomicsElement).click().perform()
 # time.sleep(5)
 #
-# #----------main logo----------#
-# mainLogoElement = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, "//img[@alt='SimpliTaught']"))
-# )
-# driver.execute_script("arguments[0].scrollIntoView();", mainLogoElement)
-# actions.move_to_element(mainLogoElement).click().perform()
-# time.sleep(5)
-#
+#----------main logo----------#
+mainLogoElement = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//img[@alt='SimpliTaught']"))
+)
+driver.execute_script("arguments[0].scrollIntoView();", mainLogoElement)
+actions.move_to_element(mainLogoElement).click().perform()
+time.sleep(5)
+
 #---------learn more about AI and ML------#
-# learnAiAndMlElement = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, "//div[@class='styles_resources__list__sLorX']//a[1]"))
-# )
-# driver.execute_script("arguments[0].scrollIntoView();", learnAiAndMlElement)
-# actions.move_to_element(learnAiAndMlElement).click().perform()
-# time.sleep(5)
-# #close icon
-# closeIconElement = WebDriverWait(driver, 10).until(
-#     EC.element_to_be_clickable((By.XPATH, "//span[@class='p-button-icon p-c pi pi-times']"))
-# )
-# driver.execute_script("arguments[0].scrollIntoView();", closeIconElement)
-# actions.move_to_element(closeIconElement).click().perform()
-# time.sleep(5)
+learnAiAndMlElement = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//div[@class='styles_resources__list__sLorX']//a[1]"))
+)
+driver.execute_script("arguments[0].scrollIntoView();", learnAiAndMlElement)
+actions.move_to_element(learnAiAndMlElement).click().perform()
+time.sleep(5)
+#close icon
+closeIconElement = WebDriverWait(driver, 10).until(
+    EC.element_to_be_clickable((By.XPATH, "//span[@class='p-button-icon p-c pi pi-times']"))
+)
+driver.execute_script("arguments[0].scrollIntoView();", closeIconElement)
+actions.move_to_element(closeIconElement).click().perform()
+time.sleep(5)
 
 test_landing_page(driver, actions, wait)
 # #----------------------Sign In----------------------------------------#
